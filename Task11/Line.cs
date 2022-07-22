17723c858693f35b04bf5daed0ad46f65476443a
эмиль ахметханов <emil.ah@yandex.ru>
2022-07-22 22:45:31 +0300

    Добавьте файлы проекта.

---
+++ Task11/Line.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11
+{
+    public struct Line
+    {
+        double b;
+        double k;
+
+        public Line(double b, double k)
+        {
+            this.b = b;
+            this.k = k;
+        }
+
+        public string Root()
+        {
+            if (k == 0)
+                return "Решений нет";
+            double r = -b / k;
+            return $"Корень равен- {r}";
+        }
+    }
+    
+    
+}
---
+++ Task11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Line;
+
+namespace Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Дано линейное уравнение kx+b=0");
+            Console.WriteLine("Найти x по известным b и k");
+            Console.WriteLine("Введите b");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите k");
+            double k = Convert.ToDouble(Console.ReadLine());
+
+            Line line = new Line(b, k);
+            Console.WriteLine(line.Root());
+            Console.ReadKey();
+        }
+    }
+}
